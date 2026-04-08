03dd1db1826836cb42c0b780a4833365c0571848
Akash-Naickar <akashnaikar2007@gmail.com>
2026-04-08 15:19:13 +0530

    style(ui): modernize timeline controls with lucide icons

--- frontend/src/components/Timeline.tsx
+++ frontend/src/components/Timeline.tsx
@@ -1,4 +1,5 @@
 import React, { useEffect, useRef } from 'react';
+import { Play, Pause } from 'lucide-react';
 
 interface TimelineProps {
   minYear: number;
@@ -59,15 +60,9 @@ export default function Timeline({
             aria-label={isPlaying ? "Pause" : "Play"}
           >
             {isPlaying ? (
-              // Pause Icon
-              <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" viewBox="0 0 20 20" fill="currentColor">
-                <path fillRule="evenodd" d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zM7 8a1 1 0 012 0v4a1 1 0 11-2 0V8zm5-1a1 1 0 00-1 1v4a1 1 0 102 0V8a1 1 0 00-1-1z" clipRule="evenodd" />
-              </svg>
+              <Pause className="w-5 h-5 fill-current" />
             ) : (
-              // Play Icon
-              <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5 ml-1" viewBox="0 0 20 20" fill="currentColor">
-                <path fillRule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zM9.555 7.168A1 1 0 008 8v4a1 1 0 001.555.832l3-2a1 1 0 000-1.664l-3-2z" clipRule="evenodd" />
-              </svg>
+              <Play className="w-5 h-5 ml-1 fill-current" />
             )}
           </button>
           
